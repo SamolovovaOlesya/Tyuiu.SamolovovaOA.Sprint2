61e791d48b1002424248bb60ce1b141ae6c631e7
Olesya Samolovova <Tyuiu.SamolovovaOA@outlook.com>
2025-10-06 16:14:19 +0500

    Add project files.

---
+++ Tyuiu.SamolovovaOA.Sprint2.Task0.V16.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.SamolovovaOA.Sprint2.Task0.V16.Lib;
+
+namespace Tyuiu.SamolovovaOA.Sprint2.Task0.V16.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int x = 1025, y = 275;
+            bool[] wait = { true, false, true, false, true, false };
+            var ds = new DataService();
+            var res = ds.GetCompareOperations(x, y);
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.SamolovovaOA.Sprint2.Task0.V16/Program.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.SamolovovaOA.Sprint2.Task0.V16.Lib;
+
+namespace Tyuiu.SamolovovaOA.Sprint2.Task0.V16;
+
+class Programm
+{
+    static void Main(string[] args) 
+    {
+        var ds = new DataService();
+
+        int x = 1025;
+        int y = 275;
+        bool[] res = new bool[6];
+        res = ds.GetCompareOperations(x, y);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("x = " + x);
+        Console.WriteLine("y = " + y);
+        Console.WriteLine();
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        for (int i = 0; i < 6; i++) 
+        {
+            Console.WriteLine(res[i]);
+        }
+        
+        Console.ReadLine();
+
+
+
+    }
+}
